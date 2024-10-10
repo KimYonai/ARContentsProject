fe56f3692e686eaf2d44e447517825a04c47d41a
KimYonai <msb0714@gmail.com>
2024-10-10 15:54:30 +0900

    [Feat] Character Touch Interaction, Touch Eat Button
    캐릭터 컨트롤러 소스코드에 캐릭터 터치 시 상호작용 기능과 먹기 버튼 터치 기능 코드를 추가했습니다.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,6 +8,10 @@ public class CharacterController : MonoBehaviour
     [SerializeField] CharacterState curState;
 
     private CreateCharacter character;
+    private Vector2 touchPos;
+
+    [Header("Model")]
+    [SerializeField] CharacterModel model;
 
     private void Start()
     {
@@ -15,7 +19,8 @@ public class CharacterController : MonoBehaviour
     }
 
     private void Update()
-    {
+    { 
+        #region State Change
         switch (curState)
         {
             case CharacterState.Idle:
@@ -42,35 +47,97 @@ public class CharacterController : MonoBehaviour
                 Die();
                 break;
         }
+        #endregion 
+    }
+
+    public void TouchAction()
+    {
+        if (Input.touchCount > 0)
+            return;
+
+        Touch touch = Input.GetTouch(0);
+        touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(touchPos, Camera.main.transform.forward);
+
+        if (hit.collider.tag == "Player" && model.Stress <= 10)
+        {
+            curState = CharacterState.Interact;
+        }
+        else if (hit.collider.tag == "Player" && model.Stress > 10 && model.Stress <= 20)
+        {
+            curState = CharacterState.Annoying;
+        }
+        else if (hit.collider.tag == "Player" && model.Stress > 20 && model.Stress <= 30)
+        {
+            curState = CharacterState.Angry;
+        }
+        else if (hit.collider.tag == "Player" && model.Stress > 30)
+        {
+            curState = CharacterState.Die;
+        }
+    }
+
+    public void TouchEatButton()
+    {
+        curState = CharacterState.Eat;
+
+        if (model.Fullness <= 80)
+        {
+            model.Stress -= 5;
+            model.Fullness += 20;
+        }
+        else if (model.Fullness > 80 &&  model.Fullness < 100)
+        {
+            model.Stress -= 5;
+            model.Fullness += 100 - model.Fullness;
+        }
+        else if (model.Fullness >= 100)
+        {
+            model.Stress += 10;
+        }
     }
 
     private void Idle()
     {
 
+
+        TouchAction();
     }
 
     private void Interact()
     {
 
+
+        TouchAction();
     }
 
     private void Annoying()
     {
 
+
+        TouchAction();
     }
 
     private void Angry()
     {
 
+
+        TouchAction();
     }
 
     private void Eat()
     {
 
+
+        TouchAction();
     }
 
     private void Die()
     {
 
+
+        Destroy(gameObject);
+        character.isSpawned = false;
     }
 }
