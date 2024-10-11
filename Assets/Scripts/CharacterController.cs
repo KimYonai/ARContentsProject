f3ac97b1dca6af80673431c52a292297e5da5434
KimYonai <msb0714@gmail.com>
2024-10-11 14:19:30 +0900

    [Fix] CharacterController Code
    기본 기능 구현을 우선적으로 진행하기 위해 캐릭터의 상태 패턴을 삭제하고 터치 쓰다듬기, 밥주기 버튼 구현 코드를 추가하는 수정 작업을 진행했습니다.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -13,131 +13,37 @@ public class CharacterController : MonoBehaviour
     [Header("Model")]
     [SerializeField] CharacterModel model;
 
-    private void Start()
-    {
-        curState = CharacterState.Idle;
-    }
-
     private void Update()
-    { 
-        #region State Change
-        switch (curState)
-        {
-            case CharacterState.Idle:
-                Idle();
-                break;
-
-            case CharacterState.Interact:
-                Interact();
-                break;
-
-            case CharacterState.Annoying:
-                Annoying();
-                break;
-
-            case CharacterState.Angry:
-                Angry();
-                break;
-
-            case CharacterState.Eat:
-                Eat();
-                break;
-
-            case CharacterState.Die:
-                Die();
-                break;
-        }
-        #endregion 
-    }
-
-    public void TouchAction()
     {
         if (Input.touchCount > 0)
-            return;
-
-        Touch touch = Input.GetTouch(0);
-        touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        RaycastHit2D hit = Physics2D.Raycast(touchPos, Camera.main.transform.forward);
-
-        if (hit.collider.tag == "Player" && model.Stress <= 10)
         {
-            curState = CharacterState.Interact;
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 touchDelta = touch.deltaPosition;
+                Vector2 screenPos = touch.position;
+
+                RaycastHit hit;
+                var pos = Camera.main.ScreenPointToRay(screenPos);
+                var ray = Physics.Raycast(pos, out hit, LayerMask.GetMask("Slime"));
+
+                //if (ray)
+                //{
+                //    if (touchDelta.x > 0)
+                //    {
+                //        hit.transform.gameObject.GetComponent<Animator>().Play();
+                //    }
+                //    else
+                //    {
+                //        hit.transform.gameObject.GetComponent<Animator>().Play();
+                //    }
+                //}
+                //else
+                //{
+                //    return;
+                //}
+            }
         }
-        else if (hit.collider.tag == "Player" && model.Stress > 10 && model.Stress <= 20)
-        {
-            curState = CharacterState.Annoying;
-        }
-        else if (hit.collider.tag == "Player" && model.Stress > 20 && model.Stress <= 30)
-        {
-            curState = CharacterState.Angry;
-        }
-        else if (hit.collider.tag == "Player" && model.Stress > 30)
-        {
-            curState = CharacterState.Die;
-        }
-    }
-
-    public void TouchEatButton()
-    {
-        curState = CharacterState.Eat;
-
-        if (model.Fullness <= 80)
-        {
-            model.Stress -= 5;
-            model.Fullness += 20;
-        }
-        else if (model.Fullness > 80 &&  model.Fullness < 100)
-        {
-            model.Stress -= 5;
-            model.Fullness += 100 - model.Fullness;
-        }
-        else if (model.Fullness >= 100)
-        {
-            model.Stress += 10;
-        }
-    }
-
-    private void Idle()
-    {
-
-
-        TouchAction();
-    }
-
-    private void Interact()
-    {
-
-
-        TouchAction();
-    }
-
-    private void Annoying()
-    {
-
-
-        TouchAction();
-    }
-
-    private void Angry()
-    {
-
-
-        TouchAction();
-    }
-
-    private void Eat()
-    {
-
-
-        TouchAction();
-    }
-
-    private void Die()
-    {
-
-
-        Destroy(gameObject);
-        character.isSpawned = false;
     }
 }
