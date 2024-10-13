38bd79f0074807fb8a072f136774a862dfc0f7a0
KimYonai <msb0714@gmail.com>
2024-10-14 00:45:17 +0900

    [Add] Audio Setting UI
    오디오 크키 및 음소거 여부를 설정할 수 있는 슬라이더와 토글 UI를 추가했습니다.
    오디오 믹서의 마스터 볼륨을 조절할 수 있도록 UI 매니저 스크립트에 소스코드를 추가했습니다.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager instance = null;
 
     private GameObject pauseWindow;
+
+    [Header("Audio")]
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider audioSlider;
+    [SerializeField] Toggle audioMute;
+    [SerializeField] GameObject speaker;
 
     public static UIManager Instance
     {
@@ -33,12 +41,26 @@ public class UIManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        audioSlider.onValueChanged.AddListener(SetAudioVolume);
+        audioMute.onValueChanged.AddListener(SetAudioMute);
     }
 
     private void Start()
     {
         pauseWindow = GameObject.Find("PauseWindow");
         pauseWindow.SetActive(false);
+
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            audioSlider.value = PlayerPrefs.GetFloat("Volume");
+        }
+        else
+            audioSlider.value = 0.5f;
+
+        audioMixer.SetFloat("Master", Mathf.Log10(audioSlider.value) * 20);
+
+        speaker.SetActive(true);
     }
 
     public void OnPauseButton()
@@ -49,6 +71,17 @@ public class UIManager : MonoBehaviour
     public void OnBackButton()
     {
         pauseWindow.SetActive(false);
+    }
+
+    public void SetAudioVolume(float volume)
+    {
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("Volume", audioSlider.value);
+    }
+
+    private void SetAudioMute(bool mute)
+    {
+        AudioListener.volume = (mute ? 0 : 1);
     }
 
     public void OnQuitButton()
